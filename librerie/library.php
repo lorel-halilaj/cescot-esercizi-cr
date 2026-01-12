71ef928517958e51f605fd1bec14866fcbded48e
Lorel <halilaj.lorel@gmail.com>
2026-01-12 12:47:39 +0100

    added new function for connecting into database in library.php

--- librerie/library.php
+++ librerie/library.php
@@ -1,5 +1,19 @@
 <?php
-function printDiv($text, $class) {
-    echo "<div class='" . $class . "'>" . $text . "</div>";
-};
+    function printDiv($text, $class) {
+        echo "<div class='" . $class . "'>" . $text . "</div>";
+    };
+
+    function connectDatabase($host = 'localhost', $username = 'root', $password = '', $dbname) {
+        $databaseHost = $host;
+        $databaseName = $dbname;
+        $databaseUsername = $username;
+        $databasePassword = $password;
+
+        $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+        //verifica la connessione
+        if (!$mysqli) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+    }
 ?>
