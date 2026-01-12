9dad71328acf7f903b5aa3f5101cc766608eee35
Lorel <halilaj.lorel@gmail.com>
2026-01-12 11:33:45 +0100

    refactor index.php to improve code readability and structure

--- db/index.php
+++ db/index.php
@@ -9,29 +9,31 @@
     <h1>
         Il testo che segue arriva dal database
     </h1>
-        <?php
-            //inizializza la connessione al database
-            $databaseHost = 'localhost';
-            $databaseName = 'cescot';
-            $databaseUsername = 'root';
-            $databasePassword = '';
+        <div>
+            <?php
+                //inizializza la connessione al database
+                $databaseHost = 'localhost';
+                $databaseName = 'cescot';
+                $databaseUsername = 'root';
+                $databasePassword = '';
 
-            $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+                $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
 
-            //verifica la connessione
-            if (!$mysqli) {
-                die("Connection failed: " . mysqli_connect_error());
-            }
+                //verifica la connessione
+                if (!$mysqli) {
+                    die("Connection failed: " . mysqli_connect_error());
+                }
 
-            //esegui una query di esempio
-            $query = 'SELECT * FROM content';
+                //esegui una query di esempio
+                $query = 'SELECT * FROM content';
 
-            $result = mysqli_query($mysqli, $query);
+                $result = mysqli_query($mysqli, $query);
 
-            //ciclo sulle righe restituite e stampo il valore di ogni riga
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo '<div><p>' . $row['value'] . '</p></div><br>';
-            }
-        ?>
+                //ciclo sulle righe restituite e stampo il valore di ogni riga
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo '<p>' . $row['value'] . '</p><br>';
+                }
+            ?>
+        </div>
 </body>
 </html>
