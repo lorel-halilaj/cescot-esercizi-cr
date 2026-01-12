e6fda3e5416e699d4fb5fefa9aa51a0d5c5361d1
Lorel <halilaj.lorel@gmail.com>
2026-01-12 11:58:48 +0100

    created new file for clients

---
+++ db/clienti/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clienti Database</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>
+        Clienti
+    </h1>
+    <?php
+        //inizializza la connessione al database
+        $databaseHost = 'localhost';
+        $databaseName = 'cescot';
+        $databaseUsername = 'root';
+        $databasePassword = '';
+
+        $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+        //verifica la connessione
+        if (!$mysqli) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        //esegui una query di esempio
+        $query = 'SELECT * FROM clienti';
+
+        $result = mysqli_query($mysqli, $query);
+
+        //ciclo sulle righe restituite e stampo il valore di ogni riga
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo '<h2>' . $row['id'] . '</h2>
+            <div>' . $row['nome'] . ' ' . $row['cognome'] . '</div>';
+        }
+    ?>
+</body>
+</html>
