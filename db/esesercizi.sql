44b9cf4195b550dd8f24218879df8b2c7f51ad19
Lorel <halilaj.lorel@gmail.com>
2026-01-13 10:50:46 +0100

    added sql file for database exercices

---
+++ db/esesercizi.sql
@@ -0,0 +1,14 @@
+SELECT cl.nome, cl.cognome, p.tipo_struttura, c.citta
+FROM clienti cl
+JOIN citta c ON c.id_citta = cl.citta
+JOIN prenotazioni p ON p.cliente = cl.id_cliente
+WHERE c.citta IN ('Bologna', 'Roma', 'Milano')
+AND p.tipo_struttura IN ('3 stelle', '4 stelle');
+
+SELECT c.citta, COUNT(*) AS totale_pren_3_4_stelle
+FROM clienti cl
+JOIN citta c ON c.id_citta = cl.citta
+JOIN prenotazioni p ON p.cliente = cl.id_cliente
+WHERE c.citta IN ('Bologna', 'Roma', 'Milano')
+AND p.tipo_struttura IN ('3 stelle', '4 stelle')
+GROUP BY c.citta;
